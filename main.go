97e16c1a852c54363b107e7017c80290b3f8bd6e
tris <tris@pop-os.localdomain>
2021-07-07 11:26:41 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Hello I am Trisdepzai")
+	// arr := []int{1, 5, 2, 3, 4, 5, 6, 3, 4, 6, 8, 4, 9, 8, 0}
+	// fmt.Println(BubbleSort(arr))
+	// fmt.Println(mergeSort(arr))
+	// fmt.Println(quicksort(arr))
+	hdl := &liteHandler{"Chào mừng đến lập trình Go cho web!"}
+	http.ListenAndServe(":8080", hdl)
+
+}
+func BubbleSort(arr []int) []int {
+	for i := 0; i < len(arr); i++ {
+		for j := 0; j < len(arr); j++ {
+			if arr[i] < arr[j] {
+				arr[i], arr[j] = arr[j], arr[i]
+			}
+		}
+	}
+	return arr
+}
+func mergeSort(items []int) []int {
+	var num = len(items)
+
+	if num == 1 {
+		return items
+	}
+
+	middle := int(num / 2)
+	var (
+		left  = make([]int, middle)
+		right = make([]int, num-middle)
+	)
+	for i := 0; i < num; i++ {
+		if i < middle {
+			left[i] = items[i]
+		} else {
+			right[i-middle] = items[i]
+		}
+	}
+
+	return merge(mergeSort(left), mergeSort(right))
+}
+
+func merge(left, right []int) (result []int) {
+	result = make([]int, len(left)+len(right))
+
+	i := 0
+	for len(left) > 0 && len(right) > 0 {
+		if left[0] < right[0] {
+			result[i] = left[0]
+			left = left[1:]
+		} else {
+			result[i] = right[0]
+			right = right[1:]
+		}
+		i++
+	}
+
+	for j := 0; j < len(left); j++ {
+		result[i] = left[j]
+		i++
+	}
+	for j := 0; j < len(right); j++ {
+		result[i] = right[j]
+		i++
+	}
+
+	return
+}
+func quicksort(a []int) []int {
+	if len(a) < 2 {
+		return a
+	}
+
+	left, right := 0, len(a)-1
+
+	pivot := rand.Int() % len(a)
+
+	a[pivot], a[right] = a[right], a[pivot]
+
+	for i, _ := range a {
+		if a[i] < a[right] {
+			a[left], a[i] = a[i], a[left]
+			left++
+		}
+	}
+
+	a[left], a[right] = a[right], a[left]
+
+	quicksort(a[:left])
+	quicksort(a[left+1:])
+
+	return a
+}
+
+type liteHandler struct {
+	message string
+}
+
+func (m *liteHandler) SeverHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, m.message)
+}
